178d106b57255e27b255e4070b8e266c2fda1b37
Peyton Kellogg <pekellogg@hotmail.com>
2022-08-31 15:40:46 -0700

    commit tree 7970a440ea28efabf8ff233258e7c552a30bd844

---
+++ index.js
@@ -0,0 +1,11 @@
+let divide = () => 2000 / 100;
+
+// let sum = (parameter1, parameter2) => {
+//   console.log(`Adding ${parameter1}`);
+//   console.log(`Adding ${parameter2}`);
+//   return parameter1 + parameter2;
+// }
+
+let square = x => x * x;
+
+let add = (x, y) => x + y;
